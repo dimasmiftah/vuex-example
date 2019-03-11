2ae246491055160fb603f0a0a0600490911a8211
Dimas Miftahul Huda <dimas.mfth@gmail.com>
2019-03-11 14:12:50 +0700

    lesson 4 - using computed properties

--- vuex-playlist/src/App.vue
+++ vuex-playlist/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <product-list-one v-bind:products="products"></product-list-one>
-    <product-list-two v-bind:products="products"></product-list-two>
+    <product-list-one></product-list-one>
+    <product-list-two></product-list-two>
   </div>
 </template>
 
